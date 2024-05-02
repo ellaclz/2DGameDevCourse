b2d373ea110b41cd1e02b63fbb53841256285c32
ellaclz <jacaluza@s.sfusd.edu>
2024-05-02 12:24:05 -0700

    M6.L5 and practices

--- M06.looting_workbook/lessons/chest/chest.gd
+++ M06.looting_workbook/lessons/chest/chest.gd
@@ -1,7 +1,12 @@
 extends Area2D
 
 @onready var canvas_group = $CanvasGroup
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+func open() -> void:
+	if animation_player.assigned_animation == "open":
+		return
+	input_pickable = false
 
 func _ready() -> void:
 	mouse_entered.connect(_on_mouse_entered)
@@ -18,3 +23,13 @@ func _on_mouse_exited() -> void:
 
 func set_outline_thickness(new_thickness: float)-> void:
 	canvas_group.material.set_shader_parameter("line_thicknes", new_thickness)
+	
+func _input_event(viewport: Node, event: InputEvent, shape_idx: int):
+	var event_is_mouse_click: bool = (
+		event is InputEventMouseButton and 
+		event.button_index == MOUSE_BUTTON_LEFT and 
+		event.is_pressed()
+	)
+	
+	if event_is_mouse_click:
+		open()
--- M06.looting_workbook/practices/L5.P1.open_the_door/door.gd
+++ M06.looting_workbook/practices/L5.P1.open_the_door/door.gd
@@ -2,12 +2,32 @@ extends Area2D
 
 @onready var animation_player: AnimationPlayer = $"AnimationPlayer"
 
+func open() -> void:
+	animation_player.play("open")
+	input_pickable = false
+
+	
+func ready() -> void:
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
+
+	
+func _on_mouse_entered() -> void:
+	var tween := create_tween()
+	tween.tween_method(set_outline_thickness, 3.0, 6.0, 0.08)
+
+func _on_mouse_exited() -> void:
+	pass
+	
+func set_outline_thickness(new_thickness: float) -> void:
+	pass
 
 func _input_event(viewport: Node, event: InputEvent, shape_index: int):
-	# Complete this boolean expression to check if the input event is a left-mouse click.
 	var event_is_mouse_click: bool = (
-		false
+		event is InputEventMouseButton and 
+		event.button_index == MOUSE_BUTTON_LEFT and 
+		event.is_pressed
 	)
 
 	if event_is_mouse_click:
-		pass
+		open()
